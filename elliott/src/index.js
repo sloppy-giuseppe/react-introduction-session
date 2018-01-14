272959447b240260dd85a63e13e3d731e4940477
Elliott Greaves <greaveselliott@me.com>
2018-01-14 19:27:12 +0000

    Added react, reactdom and webpack

---
+++ elliott/src/index.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Heading = React.createElement('h1', null, 'Hello from React!');
+
+ReactDOM.render(Heading, document.getElementById('root'));
+
