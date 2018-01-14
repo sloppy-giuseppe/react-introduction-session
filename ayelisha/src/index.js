1850fd8c0cb1364d537175c7c7420a3066849275
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2018-01-14 22:08:45 +0000

    Created react component

--- ayelisha/src/index.js
+++ ayelisha/src/index.js
@@ -1,6 +1,5 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var Heading = React.createElement('h1',null,'Hello from Webpack!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
     
-ReactDOM.render(Heading, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
