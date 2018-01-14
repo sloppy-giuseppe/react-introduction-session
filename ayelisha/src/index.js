caeb66ecf43468e09bff534c1f378043373b7d85
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2018-01-14 20:52:54 +0000

    added webpack dev server

--- ayelisha/src/index.js
+++ ayelisha/src/index.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Heading = React.createElement('h1',null,'Hello from React!');
+var Heading = React.createElement('h1',null,'Hello from Webpack!');
     
 ReactDOM.render(Heading, document.getElementById('root'));
