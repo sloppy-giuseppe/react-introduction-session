20cb4812aeec4d27bf0f79602557e035e7729715
Elliott Greaves <greaveselliott@me.com>
2018-01-14 20:52:54 +0000

    added webpack dev server

--- elliott/src/index.js
+++ elliott/src/index.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Heading = React.createElement('h1', null, 'Hello from React!');
+var Heading = React.createElement('h1', null, 'Hello from React2!');
 
 ReactDOM.render(Heading, document.getElementById('root'));
 
